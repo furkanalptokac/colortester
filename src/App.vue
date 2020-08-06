aa7cf10dd3f71f2569f37ef93660b15db616d8a9
Stajyer Furkan Alp Tokaç <furkanalptokac@gmail.com>
2020-08-06 22:41:50 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+  <h2>Test a color.</h2>
+  <input type="text"  v-model="color" maxlength="7" placeholder="#000000">
+  Color Picker: <input type="color" v-model="color">
+    <div id="color" v-bind:style="{backgroundColor: color}"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      color: '#ff9000'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#color {
+  width: 500px;
+  height: 500px;
+  margin: 0 auto;
+  margin-top: 20px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+</style>
